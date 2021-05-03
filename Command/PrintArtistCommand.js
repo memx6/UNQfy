8054d052ce3fdf57e4b9f9135d5ffcc5ee6671f6
Julian D. Trejo <62217520+TrejoJulian@users.noreply.github.com>
2021-05-03 01:44:50 -0300

    Se crearon los comandos para los prints de punta a punta

---
+++ Command/PrintArtistCommand.js
@@ -0,0 +1,18 @@
+class PrintArtistCommand {
+
+    validateInput(argumentos,unqfy){
+      if (!unqfy.hasArtistNamed(argumentos[0])){
+        throw new Error (`Command was not successful: ${argumentos[0]} is not in the system`)
+      }
+    }
+    
+    /*argumentos[0] = ArtistName,
+      the artist name must correspond to one artist from unqfy.
+      Example input: MichaelJackson*/
+    executeCommand(argumentos,unqfy){
+      this.validateInput(argumentos,unqfy)
+      let artistName = argumentos[0];
+      unqfy.printArtist(artistName);
+    }
+  }
+module.exports = PrintArtistCommand;
